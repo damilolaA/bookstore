f6e3b24ed42ea0145cd547cb5b0cce843448ea4c
dammy <adiodamilola@yahoo.com>
2018-02-14 01:56:20 +0100

    define addAdmin and interceptId controller methods

--- src/api/v1/admin/admin-model.js
+++ src/api/v1/admin/admin-model.js
@@ -1,8 +1,10 @@
 let mongoose = require('mongoose'),
   adminSchema;
 
+//create connection between mongodb and mongoose
 mongoose.connect('mongodb://mongo/bookstore');
 
+//define schema/structure for admin entity
 adminSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
@@ -10,4 +12,5 @@ adminSchema = new mongoose.Schema({
   hash: { type: String, required: true }
 });
 
+//create admin collection using adminSchema
 module.exports = mongoose.model('admin', adminSchema);
